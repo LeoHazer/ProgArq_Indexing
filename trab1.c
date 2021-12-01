06e0448c797642410035085e9e1a34888e7913f9
Leonardo Peçanha <pecanha_leonardo@id.uff.br>
2021-12-01 18:01:26 -0300

    correções com o professor

--- trab1.c
+++ trab1.c
@@ -3,10 +3,10 @@
 #include <string.h>
 #include "trab1.h"
 
-elemento *CriaLista()
-{
-    return NULL;
-}
+// elemento *CriaLista()
+// {
+//     return NULL;
+// }
 
 int Menu()
 {
@@ -15,6 +15,7 @@ int Menu()
     printf("[1]- Criar Indice para um Arquivo Texto\n");
     printf("[2]- Utilizar um Indice existente para realizar buscas por Palavras\n");
     printf("[3]- Encerrar Programa\n");
+    printf("[4]- Ocorrencia\n");
     scanf("%d", &select);
     getchar();
     return select;
@@ -26,12 +27,12 @@ void geraLinha(elemento *lst, char *linha, int contLinha, int *contPalavra)
     char *palavra  = strtok(linha, " ,.\n");
     while (palavra != NULL)
     {
-        elemento *temp = Existe(lst, palavra);
+        elemento *temp = buscaPalavra(lst, palavra);
         if (temp == lst)
         {
             Palavra plvr;
             strcpy(plvr.letras, palavra); //Copia a palavra
-            plvr.qtdOcorrencias = 1; //Ocorr�ncia inicial
+            plvr.qtdOcorrencias = 1; //Ocorrência inicial
             plvr.linhas = (int*)malloc(sizeof(int));
             plvr.linhas[0] = contLinha; //Numero da linha
             ordemAlfabetica(lst, plvr);
@@ -47,7 +48,7 @@ void geraLinha(elemento *lst, char *linha, int contLinha, int *contPalavra)
 
 }
 
-//FAZ LEITURA DO ARQUIVO E CRIA UM �NDICE
+//FAZ LEITURA DO ARQUIVO E CRIA UM INDICE
 elemento *criaIndice(char *nomeArq, int *contPalavras)
 {
 
@@ -63,73 +64,92 @@ elemento *criaIndice(char *nomeArq, int *contPalavras)
     else
     {
 
+        printf("\nLINHA TESTE\n");
         while (feof(arq) == 0)
         {
             fgets(linha, 1000, arq);
+            //puts(linha);
             geraLinha(lst, linha, contLinha, contPalavras);
             contLinha++;
         }
+        printf("\nLINHA TESTE 2\n");
         fclose(arq);
     }
     return lst;
 }
 
-//FAZ REGISTRO NO ARQUIVO BIN�RIO
+//FAZ 1 REGISTRO NO ARQUIVO BINARIO POR VEZ
 void escreveArqBinario(FILE *arq, Palavra plvr)
 {
+    printf("\nDAT 1\n");
     int sizePalavra = strlen(plvr.letras)+1;
+    printf("\nDAT 1.1\n");
     fwrite(&sizePalavra, sizeof(int), 1, arq);
+    printf("\nDAT 1.2\n");
     fwrite(plvr.letras, sizeof(char), sizePalavra, arq);
+    printf("\nDAT 1.3\n");
     fwrite(&plvr.qtdOcorrencias, sizeof(int), 1, arq);
-    fwrite(plvr.linhas, sizeof(int), plvr.qtdOcorrencias,arq);
+    printf("\nDAT 1.4\n");
+    fwrite(plvr.linhas, sizeof(int), plvr.qtdOcorrencias, arq);
+    printf("\nDAT 2\n");
 }
 
-//FAZ LEITURA DE PALAVRA NO ARQUIVO BIN�RIO
+//FAZ LEITURA DE PALAVRA NO ARQUIVO BINARIO
 Palavra readBinario(FILE *arq)
 {
+
     Palavra novo;
     int sizePalavra;
+    printf("\nread_bin1\n");
     fread(&sizePalavra, sizeof(int), 1, arq);
     fread(novo.letras, sizeof(char), sizePalavra, arq);
     fread(&novo.qtdOcorrencias, sizeof(int), 1, arq);
-    novo.linhas = (int*) malloc(sizeof(int)*novo.qtdOcorrencias);
+    novo.linhas = (int*) malloc(novo.qtdOcorrencias * sizeof(int));
     fread(novo.linhas,sizeof(int),novo.qtdOcorrencias,arq);
+    printf("\nread_bin2\n");
     return novo;
 }
 
-//REGISTRA �NDICE NO BIN�RIO
+//REGISTRA INDICE NO BINARIO
 void escreveBinario(elemento *lst, int contPalavra)
 {
+    printf("\nBIN 1\n");
     FILE *arq = fopen("indice.dat", "wb");
 
+    printf("\nBIN 2\n");
     fwrite(&contPalavra, sizeof(int), 1, arq);
 
+    printf("\nBIN 3\n");
     elemento *temp = lst->prox;
     while (temp!= lst)
     {
+        printf("\nBIN 4\n");
         escreveArqBinario(arq, temp->word);
         temp = temp->prox;
+        printf("\nBIN 5\n");
     }
     fclose(arq);
+    printf("\nBIN 6\n");
 }
 
-//INSERE NO FIM DA LISTA O ARQUIVO BIN�RIO LIDO
+//INSERE NO FIM DA LISTA O ARQUIVO BINARIO LIDO
 elemento *insereBinFim(int *contPalavra)
 {
+    printf("\nB_FIM1\n");
     elemento *lst = CriaLista();
+    printf("\nB_FIM2\n");
     FILE *arq = fopen("indice.dat", "rb");
+    printf("\nB_FIM3\n");
     fread(contPalavra, sizeof(int), 1, arq);
     for (int i = 0; i < (*contPalavra); i++)
     {
+        printf("\nB_FIM4\n");
         insereFim(lst, readBinario(arq));
     }
+    printf("\nB_FIM5\n");
     fclose(arq);
     return lst;
 }
-
-
-
-
 
 
 int main()
@@ -137,52 +157,74 @@ int main()
 
     char nArquivo[80], palvra[80];
     elemento *lista = CriaLista();
+    elemento *aux;
     int contPalavra =0;
     int escolhe;
 
-    while ( (escolhe = Menu() != 3) )
+    do
     {
+
+        escolhe = Menu();
+
         switch (escolhe)
         {
         case 1:
+
+            destroiIndice(lista);
             printf("Digite o nome do arquivo txt: ");
             fgets(nArquivo, 80, stdin);
 
             if(nArquivo[strlen(nArquivo)-1]=='\n') nArquivo[strlen(nArquivo)-1]='\0';
             fflush(stdin);
 
+            printf("\nTESTE1\n");
             lista = criaIndice(nArquivo, &contPalavra);
-            Listar(lista);
+            printf("\nTESTE2\n");
+            printf("\nTESTE3\n");
             escreveBinario(lista, contPalavra);
+            printf("\nTESTE4\n");
+            printf("Index criado com sucesso!!\n\n");
+            Listar(lista);
             break;
+
         case 2:
             /***/
+            system("cls");
+            //destroiIndice(lista);
             lista = insereBinFim(&contPalavra);
-            printf("Insira a palavra para busca: \n");
-            fgets(palvra, 80, stdin);
+            printf("Informe a palavra para buscar.\n");
             fflush(stdin);
+            scanf("%s", palvra);
 
-            elemento *tmp = Existe(lista, palvra);
-            if (tmp == lista)
+            aux = buscaPalavra(lista, palvra);
+            if(aux == lista)
             {
-                printf(" %s nao encontrada!\n",palvra);
+                printf("[%s] nao foi encontrada!\n ", palvra);
             }
             else
             {
-                mostraOcorrencia(tmp->word);
+                mostraOcorrencia(aux->word);
             }
+
             break;
+
         case 3:
             DestruirLista(lista);
+            exit(-1);
+            break;
 
-
+        case 4:
+            system("cls");
+            //destroiIndice(lista);
+            Listar(lista);
+            break;
 
         default:
             fprintf(stderr, "Opcao invalida!\n");
             break;
         }
     }
-
+    while (escolhe >=1 && escolhe <=5);
 
 
     return 0;
